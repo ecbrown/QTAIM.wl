a0be4e56a9a946df1698a18fe02e8a17b1a6daa3
Eric Brown <ecbrown@ericcbrown.com>
2021-07-07 13:57:26 -0500

    Tune AssociatedNuclearAttractor, revert to Adams

--- QTAIM/QTAIM/QTAIM.m
+++ QTAIM/QTAIM/QTAIM.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72c16f7d98d7083c84b962307e01cbeffce7a46a0f93429588cf49dea6d4befa
-size 42043
+oid sha256:85460c2de0ec90dce87a9088e3a7fac78974a79cc9524f6dcaabab37d65b222d
+size 42556
