ac32be7a87973f645cdd7c6614a280a5c506fcd4
Eric Brown <ecbrown@ericcbrown.com>
2021-07-07 19:41:44 -0500

    Enable Mathematica-native optimized rho and gradient routines

--- QTAIM/QTAIM/QTAIM.m
+++ QTAIM/QTAIM/QTAIM.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85460c2de0ec90dce87a9088e3a7fac78974a79cc9524f6dcaabab37d65b222d
-size 42556
+oid sha256:b653fa84bf2ca85027a961ed961ca878e65b9a1cd69aa560c103868d5e29661b
+size 51287
