2f92119cd33ef364cc25a0aaff7e101dab78dba4
Eric Brown <ecbrown@ericcbrown.com>
2021-07-07 20:09:41 -0500

    Release v1.0.0

--- QTAIM/PacletInfo.m
+++ QTAIM/PacletInfo.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91a71e828902fe04d233b2bee566e9d19da973efa696358ae2ef97b938a77d6f
+oid sha256:fca501811850a2e1cf6c3026f7282ffaf2671960d5226541d4f15f7b1a2e6fcf
 size 332
