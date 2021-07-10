94633918e7cb668caca0c810b09eb30dfb0c0b68
Eric Brown <ecbrown@ericcbrown.com>
2021-07-10 14:50:18 -0500

    Better cutoffs in Fortran

--- QTAIM/QTAIM/QTAIM.m
+++ QTAIM/QTAIM/QTAIM.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e5b4f7d5f6a6ddb218c6f0cca14237c18826e12207545e3e752bd57e75aeab
-size 52646
+oid sha256:f44721276029e26b9bb193ff7a3b5f29087e334654ef8594f573b021ec43830a
+size 52791
--- QTAIM/QTAIM/qtaim.f90
+++ QTAIM/QTAIM/qtaim.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893df4aa3ffd4c2dfe966497cd1bb7da5c2db056659c108dade0665d3c554056
-size 14984
+oid sha256:b8053ec696d3aa3f794f8643ba0c4b034ca0897659f8c4aa57a2c722b36c2d75
+size 15683
