3b4e941e9fa834604f0ae7b0139d6d1a785d4bb4
Eric Brown <ecbrown@ericcbrown.com>
2021-07-11 13:29:26 -0500

    Changes for large molecules

--- QTAIM/QTAIM/QTAIM.m
+++ QTAIM/QTAIM/QTAIM.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f44721276029e26b9bb193ff7a3b5f29087e334654ef8594f573b021ec43830a
-size 52791
+oid sha256:cab43a8b15f18f9ba1c8720ec331c941f28cb5f79813c77b8ac4484695cdcfc6
+size 53085
--- QTAIM/QTAIM/qtaim.f90
+++ QTAIM/QTAIM/qtaim.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8053ec696d3aa3f794f8643ba0c4b034ca0897659f8c4aa57a2c722b36c2d75
-size 15683
+oid sha256:7bb31c246c2a5c6e6d734a326fa5c855cdc826f6fac5e241639244b85b50cb91
+size 15626
